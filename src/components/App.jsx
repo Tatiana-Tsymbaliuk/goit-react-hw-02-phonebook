193808294af7a1a8bc71bf664dbbbe5a7cf31ee5
Tatiana-Tsymbaliuk <114348739+Tatiana-Tsymbaliuk@users.noreply.github.com>
2023-05-09 20:01:05 +0300

    create form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+
+import React from 'react';
+
+import FormPhonebook from 'components/FormPhonebook/FormPhonebook'
+
+export class App extends React.Component {
+  // state = {
+  //   contacts: [],
+  //   name: ''
+  // }
+
+
+  render() {
+ 
+     return <div>
+  <h1>Ponebook</h1>
+   <FormPhonebook/>
+     
+
+<div>
+  <h2>Contacts</h2>
+  <ul>
+    <li>{this.state.name}</li>
+  </ul>
+</div>
     </div>
-  );
+  }
 };
+
